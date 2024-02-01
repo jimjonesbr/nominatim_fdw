a102bb362067b075429629d05d00d082865f9b27
Jim Jones <jim.jones@uni-muenster.de>
2024-02-01 08:54:43 +0100

    Update regression tests and .gitignore

--- sql/functions.sql
+++ sql/functions.sql
@@ -5,10 +5,24 @@ OPTIONS (url 'https://nominatim.openstreetmap.org');
 SELECT * FROM nominatim_fdw_version();
 
 SELECT 
-    osm_id, osm_type, ref, class, display_name, display_rank, place_id IS NULL, place_rank,
-    lon, lat, boundingbox, importance, icon, timestamp IS NOT NULL AS ts, attribution,
-    querystring, length(polygon) AS polygon_length, exclude_place_ids, more_url,
-    jsonb_pretty(extratags) AS extratags, jsonb_pretty(namedetails) AS namedetails,
+    osm_id, osm_type, 
+	ref, 
+	class, 
+	display_name, 
+	display_rank, 
+	place_id IS NOT NULL AND place_id > 0 AS valid_place_id, 
+	place_rank,
+    lon, lat, boundingbox, 
+	importance, 
+	icon, 
+	timestamp IS NOT NULL AS valid_timestamp, 
+	attribution,
+    querystring, 
+	length(polygon) AS polygon_length, 
+	exclude_place_ids IS NOT NULL AS valid_exclude_place_ids, 
+	more_url IS NOT NULL AND more_url <> '' AS valid_more_url,
+    jsonb_pretty(extratags) AS extratags, 
+	jsonb_pretty(namedetails) AS namedetails,
     jsonb_pretty(addressdetails) AS addressdetails
 FROM nominatim_search(
       server_name => 'osm',
@@ -19,22 +33,37 @@ FROM nominatim_search(
       polygon => 'polygon_text',
       email => 'jim.jones@uni-muenster.de',
       countrycodes => 'DE,BR,US',
-      featuretype => 'office',
+      featuretype => 'poi',
       dedupe => true,
       exclude_place_ids => '42,73',
       viewbox => '51.9659397,51.9661584,7.6036345,7.6039893',
       polygon_threshold => 0.1,
       layer => 'address',
       limit_result => 1,
+      bounded => false,
       accept_language => 'de_DE,de,q=0.9');
 
 SELECT pg_sleep(2);
 
 SELECT 
-    osm_id, osm_type, ref, class, display_name, display_rank, place_id IS NULL, place_rank,
-    lon, lat, boundingbox, importance, icon, timestamp IS NOT NULL AS ts, attribution,
-    querystring, length(polygon) AS polygon_length, exclude_place_ids, more_url,
-    jsonb_pretty(extratags) AS extratags, jsonb_pretty(namedetails) AS namedetails,
+    osm_id, osm_type, 
+	ref, 
+	class, 
+	display_name, 
+	display_rank, 
+	place_id IS NOT NULL AND place_id > 0 AS valid_place_id, 
+	place_rank,
+    lon, lat, boundingbox, 
+	importance, 
+	icon, 
+	timestamp IS NOT NULL AS valid_timestamp, 
+	attribution,
+    querystring, 
+	length(polygon) AS polygon_length, 
+	exclude_place_ids IS NOT NULL AS valid_exclude_place_ids, 
+	more_url IS NOT NULL AND more_url <> '' AS valid_more_url,
+    jsonb_pretty(extratags) AS extratags, 
+	jsonb_pretty(namedetails) AS namedetails,
     jsonb_pretty(addressdetails) AS addressdetails
 FROM nominatim_search(
       server_name => 'osm',
@@ -62,10 +91,21 @@ FROM nominatim_search(
 SELECT pg_sleep(2);
 
 SELECT 
-    osm_id, osm_type, result, ref, place_id IS NULL, place_rank, lon, lat, boundingbox, 
-    icon, timestamp IS NOT NULL AS ts, attribution, querystring, 
-    length(polygon) AS polygon_length, jsonb_pretty(extratags) AS extratags, 
-    jsonb_pretty(namedetails) AS namedetails, jsonb_pretty(addressparts) AS addressparts
+    osm_id, 
+	osm_type, 
+	result, 
+	ref, 
+	place_id IS NOT NULL AND place_id > 0 AS valid_place_id, 
+	place_rank, 
+	lon, lat, boundingbox, 
+    icon, 
+	timestamp IS NOT NULL AS valid_timestamp, 
+	attribution, 
+	querystring, 
+    length(polygon) AS polygon_length, 
+	jsonb_pretty(extratags) AS extratags, 
+    jsonb_pretty(namedetails) AS namedetails, 
+	jsonb_pretty(addressparts) AS addressparts
 FROM nominatim_reverse(
         server_name => 'osm', 
         lon => 7.6038115,
@@ -77,13 +117,25 @@ FROM nominatim_reverse(
         accept_language => 'de_DE,de,q=0.9');
 
 SELECT pg_sleep(2);
-
+		
 SELECT 
-    osm_id, osm_type, ref, class, display_name, display_rank, place_id IS NULL, place_rank,
-    lon, lat, boundingbox, importance, icon, timestamp IS NOT NULL AS ts, attribution,
-    querystring, length(polygon) AS polygon_length, exclude_place_ids, more_url,
-    jsonb_pretty(extratags) AS extratags, jsonb_pretty(namedetails) AS namedetails,
-    jsonb_pretty(addressdetails) AS addressdetails 
+    osm_id, osm_type, 
+	ref, 
+	class, 
+	display_name, 
+	display_rank, 
+	place_id IS NOT NULL AND place_id > 0 AS valid_place_id, 
+	place_rank,
+    lon, lat, boundingbox, 
+	importance, 
+	icon, 
+	timestamp IS NOT NULL AS valid_timestamp, 
+	attribution,
+    querystring, 
+	length(polygon) AS polygon_length, 
+    jsonb_pretty(extratags) AS extratags, 
+	jsonb_pretty(namedetails) AS namedetails,
+    jsonb_pretty(addressdetails) AS addressdetails
 FROM nominatim_lookup(
       server_name => 'osm',
       osm_ids => 'W88291927',
@@ -99,4 +151,4 @@ FROM nominatim_lookup(
       viewbox => '51.9659397,51.9661584,7.6036345,7.6039893',
       polygon_threshold => 0.1,
       layer => 'address',
-      accept_language => 'de_DE,de,q=0.9');
+      accept_language => 'de_DE,de,q=0.9');		
