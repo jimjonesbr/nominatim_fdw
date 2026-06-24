55e07b9b47a6726f8bc10acd0a7e70c1c2c5c77c
Jim Jones <jim.jones@uni-muenster.de>
2026-06-24 06:11:23 +0200

    Update test scripts
    This replaces docker with podman for easier testing in RHEL

--- scripts/pg18-env/deploy-pg18-env.sh
+++ scripts/pg18-env/deploy-pg18-env.sh
@@ -4,12 +4,12 @@ PG_CONTAINER=nominatim_pg18
 NETWORK_NAME=pgnet
 IP_ADDRESS="172.19.42.218"
 
-docker build -t $POSTGRES18_IMAGE .
+podman build -t $POSTGRES18_IMAGE .
 
-docker stop $PG_CONTAINER
-docker rm $PG_CONTAINER
-docker network create --driver=bridge --subnet=172.19.42.0/24 $NETWORK_NAME
-docker run -d \
+podman stop $PG_CONTAINER
+podman rm $PG_CONTAINER
+podman network create --driver=bridge --subnet=172.19.42.0/24 $NETWORK_NAME
+podman run -d \
   --name   $PG_CONTAINER \
   --network $NETWORK_NAME \
   --env POSTGRES_HOST_AUTH_METHOD=trust \
@@ -17,6 +17,6 @@ docker run -d \
   --volume  $NOMINATIM_FDW_PATH:/nominatim_fdw:Z \
   $POSTGRES18_IMAGE -c logging_collector=on &&
 
-docker exec -itw /nominatim_fdw/ $PG_CONTAINER make clean &&
-docker exec -itw /nominatim_fdw/ $PG_CONTAINER make PG_CONFIG=/usr/lib/postgresql/18/bin/pg_config &&
-docker exec -itw /nominatim_fdw/ $PG_CONTAINER make install
+podman exec -itw /nominatim_fdw/ $PG_CONTAINER make clean &&
+podman exec -itw /nominatim_fdw/ $PG_CONTAINER make PG_CONFIG=/usr/lib/postgresql/18/bin/pg_config &&
+podman exec -itw /nominatim_fdw/ $PG_CONTAINER make install
--- scripts/pg18-env/run-tests-pg18.sh
+++ scripts/pg18-env/run-tests-pg18.sh
@@ -6,15 +6,15 @@ NETWORK_NAME=pgnet
 # Build and install nominatim_fdw
 echo -e "\n== Building and Installing nominatim_fdw on PostgreSQL 18 ==\n"
 
-docker exec -itw /nominatim_fdw/ $CONTAINER_NAME make uninstall 2>/dev/null || true
-docker exec -itw /nominatim_fdw/ $CONTAINER_NAME make clean
-docker exec -itw /nominatim_fdw/ $CONTAINER_NAME make
-docker exec -itw /nominatim_fdw/ $CONTAINER_NAME make install
-docker restart $CONTAINER_NAME
-docker exec -itw /nominatim_fdw/ -u postgres $CONTAINER_NAME psql -d postgres \
+podman exec -itw /nominatim_fdw/ $CONTAINER_NAME make uninstall 2>/dev/null || true
+podman exec -itw /nominatim_fdw/ $CONTAINER_NAME make clean
+podman exec -itw /nominatim_fdw/ $CONTAINER_NAME make
+podman exec -itw /nominatim_fdw/ $CONTAINER_NAME make install
+podman restart $CONTAINER_NAME
+podman exec -itw /nominatim_fdw/ -u postgres $CONTAINER_NAME psql -d postgres \
   -c "DROP EXTENSION IF EXISTS nominatim_fdw CASCADE; CREATE EXTENSION nominatim_fdw"
 
 # SKIP_PROXY_TESTS=1 - skip proxy tests since we don't have a proxy set up in this environment
 
-docker exec -itw /nominatim_fdw/ $CONTAINER_NAME make PGUSER=postgres SKIP_PROXY_TESTS=1 installcheck 
+podman exec -itw /nominatim_fdw/ $CONTAINER_NAME make PGUSER=postgres SKIP_PROXY_TESTS=1 installcheck 
 echo -e "\n== Tests completed ==\n"
